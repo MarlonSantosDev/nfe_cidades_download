a9a18b44c0167bb4401707cc0734012a88bf2e3c
Marlon Santos <marlon-20-12@hotmail.com>
2025-12-23 22:56:42 -0300

    0.0.1

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -3,15 +3,12 @@ class NfeConstants {
   NfeConstants._();
 
   // Anti-Captcha endpoints
-  static const antiCaptchaCreateTaskUrl =
-      'https://api.anti-captcha.com/createTask';
-  static const antiCaptchaGetResultUrl =
-      'https://api.anti-captcha.com/getTaskResult';
+  static const antiCaptchaCreateTaskUrl = 'https://api.anti-captcha.com/createTask';
+  static const antiCaptchaGetResultUrl = 'https://api.anti-captcha.com/getTaskResult';
 
   // NFe-Cidades endpoints
   static const nfeBaseUrl = 'https://www.nfe-cidades.com.br';
-  static const nfeBuscarDocumentoUrl =
-      '$nfeBaseUrl/v2/rest/public/login/buscarDocumento';
+  static const nfeBuscarDocumentoUrl = '$nfeBaseUrl/v2/rest/public/login/buscarDocumento';
   static const nfeDocumentosUrl = '$nfeBaseUrl/documentos.action';
   static const nfeRelatorioUrl = '$nfeBaseUrl/relatorioNotaFiscal.action';
   static const nfeLandingPage = '$nfeBaseUrl/landing-page';
@@ -20,8 +17,7 @@ class NfeConstants {
   static const recaptchaSiteKey = '6Lf9374hAAAAAMorFLzMzomJWlbu0FK92Q25culn';
 
   // HTTP headers
-  static const userAgent =
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:146.0) Gecko/20100101 Firefox/146.0';
+  static const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:146.0) Gecko/20100101 Firefox/146.0';
 
   // Timeouts and intervals
   static const defaultTimeout = Duration(minutes: 1);
